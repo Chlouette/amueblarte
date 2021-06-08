b9279793d81e80a120921cc20ac7f7efb4a65a3f
Veni Aljo <veni.aljo@gmail.com>
2021-06-08 15:06:24 +0100

    generated favorite_items controller and view folder

---
+++ app/controllers/favorite_items_controller.rb
@@ -0,0 +1,2 @@
+class FavoriteItemsController < ApplicationController
+end
---
+++ test/controllers/favorite_items_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FavoriteItemsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
