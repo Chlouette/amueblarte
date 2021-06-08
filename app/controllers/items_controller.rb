14c0c3e57dbb058d84835e8e4b04bdb26a426260
James Price <jamesmpdev@gmail.com>
2021-06-08 17:19:17 +0100

    fixing add new item form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,15 @@ class ItemsController < ApplicationController
     end
 
     def new 
-        @item = Item.new
+        @item = Item.new()
     end
 
     def create
         @item = Item.new(item_params)
+        @item.user = current_user
         if @item.save
+            # for now redirect to home
+            redirect_to root_path
             # redirect_to new_booking_path(@item)
         else
             render :new
@@ -24,14 +27,14 @@ class ItemsController < ApplicationController
         @item = Item.find(params[:id])
     end
     
-
     def update
         @item = Item.find(params[:id])
+        @item.save
     end
 
     private
 
     def item_params
-        params.require(:item).permit(:name, :description, photos: [])
+        params.require(:item).permit(:name, :description, :category, photos: [])
     end
 end
