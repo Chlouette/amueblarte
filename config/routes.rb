9a3c76a820781d0552e17038ec5a95ddda66e5f3
Veni Aljo <veni.aljo@gmail.com>
2021-06-08 15:23:22 +0100

    added route to create a new favorite_item and defined create method in controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users, only: [:show]
   resources :artists, only: [:index, :show]
+  resources :favorite_items, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
