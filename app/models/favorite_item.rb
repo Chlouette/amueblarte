be989086565ada3b4dbde55c1ef710f48272a355
Veni Aljo <veni.aljo@gmail.com>
2021-06-08 12:15:30 +0100

    inputted basic validations on item and artist models

--- app/models/favorite_item.rb
+++ app/models/favorite_item.rb
@@ -1,7 +1,4 @@
 class FavoriteItem < ApplicationRecord
   belongs_to :user
   belongs_to :item
-
-  validates :name, :description, :category, presence: true
-  validates
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
 
   has_many :bookings
   has_many :favorite_items
+
+  validates :name, :description, :category, presence: true
 end
