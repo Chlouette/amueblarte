fc5613eee48518bc76a6845968bb775eec71d2da
James Price <jamesmpdev@gmail.com>
2021-06-08 14:56:56 +0100

    Creating items controller. adding show page to items controller + routes.

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,17 @@
+class ItemsController < ApplicationController
+    def index
+        @items = Item.all
+    end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    def new 
+        @item = Item.new
+    end
+
+    def create
+        
+    end
+end
