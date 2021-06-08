fdfd0efacb50022cc7e75583bffe6fda4b02342c
Veni Aljo <veni.aljo@gmail.com>
2021-06-08 13:11:26 +0100

    artists routes and views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,9 @@
 class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
 
-def index
-  @artists = Artist.all
-end
-
+  def show
+    @artist = Artist.find(params[:id])
+  end
 end
