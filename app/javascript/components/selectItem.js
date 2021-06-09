638d6d6ec16fae78427c931ba15d687a58dc3fb1
James Price <jamesmpdev@gmail.com>
2021-06-09 16:41:57 +0100

    javascript select works like a radio button. Will deselect class when clicking on new item

--- app/javascript/components/selectItem.js
+++ app/javascript/components/selectItem.js
@@ -1,11 +1,16 @@
+const items = document.querySelectorAll(".item-card");
+
 const changeNextBtnHref = (event) => {
 //   console.log(event.currentTarget);
   const nextBtn = document.getElementById('nextBtn');
   nextBtn.href = event.target.dataset.href
+  items.forEach(item => {
+    item.classList.remove('selected')
+  })
+  event.target.classList.add('selected')
 };
 
 const bindButtonToClick = () => {
-  const items = document.querySelectorAll(".item-card");
   items.forEach(item => {
       item.addEventListener("click", changeNextBtnHref);
   });
