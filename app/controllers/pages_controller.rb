338cf4552ae2bde2e03c6332cf7787e20c675824
Veni Aljo <veni.aljo@gmail.com>
2021-06-10 16:30:35 +0100

    homepage with nokogiri cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,30 @@
+require 'open-uri'
+require 'nokogiri'
+
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @articles = []
+    @images = []
+    @links = []
+
+    url = 'https://www.livingetc.com/news'
+
+    html_file = URI.parse(url).open.read
+    html_doc = Nokogiri::HTML(html_file)
+
+    html_doc.search('.listing__title').each do |element|
+      @articles << element.text.strip
+    end
+
+    html_doc.search('.lazy-image-van').each do |element|
+      @images << element.attribute('data-original-mos').value
+    end
+
+    html_doc.search('.listing__link').each do |element|
+      @links << element.attribute('href').value
+    end
+    @images.uniq!
   end
 end
