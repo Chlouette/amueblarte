d42961d202d1db8506adc1152e118cbc19aa286c
James Price <jamesmpdev@gmail.com>
2021-06-10 13:17:53 +0100

    adding seed file with collected status items and logic to get background img of cards as cloudinary imgs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require "open-uri"
 require 'faker'
 
 puts "Cleaning database..."
@@ -27,14 +27,6 @@ item_category = [
   "Side Table", "Lamp", "Coat Stand",
   "Shelves", "Wine Rack", "Coffee Table"
 ]
-
-item_description = 
-  "Contemporary designers explore new technologies, experiment with materials, 
-  and rely on cutting-edge fabrication techniques. Zaha Hadid and Frank Gehry 
-  create works that are conceived in virtual modeling programs and fabricated using 
-  modern techniques such as 3D printing and laser cutting. Others are part of a 
-  shift toward DIY, with designers like Wendell Castle emphasizing a return to 
-  hand-craftsmanship and sculptural form in their work."
 
 status = [
   "to be collected",
@@ -64,14 +56,145 @@ puts "Creating Artists..."
 }
 
 puts "Creating Items..."
+# create items method. just plug in the correct data
+  def createItem(img_url, category, name, item_description, item_color, item_status)
+    item = Item.new(
+      category: category,
+      name: name,
+      description: item_description,
+      color: item_color,
+      status: item_status,
+      user: User.all.sample
+    )
+    img = URI.open(img_url)
+    item.photos.attach(io: img, filename: "#{name}.png", content_type: 'image/png')
+    item.save
+  end
 
-15.times {
-  Item.create!(
-    category: item_category.sample,
-    name: "Contemporary upholstered bench with wood detail",
-    description: item_description,
-    color: Faker::Color.color_name,
-    status: status.sample,
-    user: User.all.sample
-  )
-}
+  puts "Creating Collected items"
+  # CHAIRS
+    createItem(
+      'https://i.ebayimg.com/00/s/NzY4WDEwMjQ=/z/l6wAAOSwhWxgudc5/$_86.JPG',
+      'Chair', 
+      'Riser/recliner chair',
+      'Electric riser /recliner armchair. In excellent condition. Bought about ten months ago but only used for about three',
+      'Beige',
+      'collected' 
+    )
+
+    createItem(
+      'https://i.ebayimg.com/00/s/MTAyNFg3Njg=/z/WYkAAOSwh-ZgiTCI/$_86.JPG',
+      'Chair', 
+      'Lovely vintage chair',
+      'Beautiful vintage chair
+
+      This has been painted in dark grey and then dry brushed with a lighter grey to give it the most amazing texture. It looks like it’s made of stone.',
+      'Dark Grey',
+      'collected' 
+    )
+
+    createItem(
+      'https://i.ebayimg.com/00/s/NjAwWDgwMA==/z/728AAOSwFOZgwNBG/$_86.PNG',
+      'Chair', 
+      'Pair of old chairs',
+      'Two dining chairs with coil sprung/webbing. In reasonable condition.',
+      'Brown',
+      'collected' 
+    )
+
+    # TABLES
+    createItem(
+      'https://i.ebayimg.com/00/s/NTc2WDEwMjQ=/z/TWwAAOSwf0ZguMYu/$_86.JPG',
+      'Table', 
+      'Old Sewing table',
+      'Solid wood top on old sewing table metal frame with pedal. Rubber pads at end of legs.
+
+      Table in excellent condition',
+      'Brown',
+      'collected' 
+    )
+
+    createItem(
+      'https://i.ebayimg.com/00/s/NzY1WDEwMjQ=/z/V1YAAOSwna5gnjSg/$_86.JPG',
+      'Table', 
+      'Nested Tables',
+      'Oak veneer nest of tables with glass insets. The smaller of the two is damaged which could be repaired with a new veneer strip.',
+      'Brown',
+      'collected' 
+    )
+
+    createItem(
+      'https://i.ebayimg.com/00/s/MTAyNFg4MjI=/z/i7kAAOSwfWtgwfMJ/$_86.JPG',
+      'Table', 
+      'Adjustable Round Table',
+      'Some chips and scratches
+
+      Apart from that in solid and sturdy condition and structurally sound',
+      'Metallic, Black',
+      'collected' 
+    )
+
+    createItem(
+      'https://i.ebayimg.com/00/s/MTAyNFg1NzY=/z/DJQAAOSw6m1gwfHA/$_86.JPG',
+      'Table', 
+      'Nest of 3 Glass Top Coffee Tables',
+      'Nest of 3 tables, mahogany colour wood, gold inlay and glass inset. Stack beautifully, 3 very handy sizes. Slight damage on base of middle sized one at base, shown in photo.',
+      'Mahogany',
+      'collected' 
+    )
+
+    # DRAWERS
+
+    createItem(
+      'https://i.ebayimg.com/00/s/MTAyNFg0NzM=/z/QGAAAOSwUP1gwcAt/$_86.JPG',
+      'Drawers', 
+      'Chest of Drawers',
+      'Pine Chest of Drawers. Sturdy made, good quality drawers. No damage, has been used, few areas needs rubbing down and rewaxing.',
+      'Light Brown',
+      'collected' 
+    )
+
+    # COAT STANDS
+
+    createItem(
+      'https://i.ebayimg.com/00/s/MTAyNFg3Njg=/z/CnAAAOSw~QNguLrx/$_86.JPG',
+      'Coat Stand', 
+      'Hat/Coat Stand',
+      'The perfect way to store your hat collection or just add a little extra character to your hallway, and somewhere to put those coats when you come in.',
+      'Mahogany',
+      'collected'
+    )
+
+    createItem(
+      'https://i.ebayimg.com/00/s/MTAyNFg3Njg=/z/uAYAAOSwX4dgwesB/$_86.JPG',
+      'Coat Stand', 
+      'Coat Stand',
+      'Solid wood coat stand, really great condition.
+      Top part can lift off as shown so can fit in a car!',
+      'Light Brown',
+      'collected'
+    )
+
+    # STOOLS
+
+    createItem(
+      'https://i.ebayimg.com/00/s/MTAyNFg3Njg=/z/b2oAAOSw2ZNgvysr/$_86.JPG',
+      'Stool', 
+      'Piano Stool',
+      'Fine mahogany piano stool, recently re-upholstered. Very comfortable slightly angled seat, perfect for playing the piano. Music storage inside.',
+      'Mahogany',
+      'collected'
+    )
+
+    createItem(
+      'https://i.ebayimg.com/00/s/ODAwWDYwMA==/z/PegAAOSws7dgwfjD/$_86.PNG',
+      'Stool', 
+      'Storage Stool',
+      'DFS Storage foot stool in very good condition, taking up too much room and not using it for any reason.',
+      'Grey',
+      'collected'
+    )
+  
+  puts "Collected items create"
+  
+  
