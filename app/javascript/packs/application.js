511d939bb285fa58a8d953422cd1f836fb43956f
James Price <jamesmpdev@gmail.com>
2021-06-10 14:18:00 +0100

    cleaning up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,8 +35,7 @@ import { randomiseBorderColor } from "../components/randomColor";
 
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+
   if (document.getElementsByClassName("item-card")) {
     bindButtonToClick()
   }
