70ddbe1a83d773f8defd5c76f0cd60deec658028
Chloe Avenas <avenaschloe@gmail.com>
2021-06-10 16:31:32 +0100

    changed flatpickr to have either start_date or start_date+end_date

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,12 +1,25 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {
-    minDate: "today",
-    altInput: true,
-    altFormat: "F j, Y",
-    dateFormat: "Y-m-d"
-  });
+  const datepicker = document.querySelector(".datepicker");
+  let options
+  if (datepicker.dataset.multiple === "true") {
+    options = {
+      minDate: "today",
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d",
+      mode: "range"
+    }
+  } else {
+    options = {
+      minDate: "today",
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d"
+    }
+  }
+  flatpickr(".datepicker", options);
 }
 
 export { initFlatpickr };
