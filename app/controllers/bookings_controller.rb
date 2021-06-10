f432e20e2471dc15a2e2092fa5b5049e61453ac9
Chloe Avenas <avenaschloe@gmail.com>
2021-06-10 12:09:56 +0100

    added creation and decoration routes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -25,6 +25,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
