4347e0671b9e06af71ecf164c75414f39b91a1dc
James Price <jamesmpdev@gmail.com>
2021-06-11 14:45:01 +0100

    adding wrapped js function to fix turbolinks

--- app/javascript/components/selectItem.js
+++ app/javascript/components/selectItem.js
@@ -1,23 +1,27 @@
 import { randomiseBorderColor } from "./randomColor";
-
-const items = document.querySelectorAll(".item-card");
-
-const changeNextBtnHref = (event) => {
-//   console.log(event.currentTarget);
-  const nextBtn = document.getElementById('nextBtn');
-  nextBtn.href = event.target.dataset.href
-  items.forEach(item => {
-    item.classList.remove('selected')
-    item.style.borderColor = 'black'
-  })
-  event.target.classList.add('selected')
-  randomiseBorderColor(event.target)
-};
-
-const bindButtonToClick = () => {
-  items.forEach(item => {
+const selectItem = () => {
+  
+  const items = document.querySelectorAll(".item-card");
+  
+  const changeNextBtnHref = (event) => {
+    //   console.log(event.currentTarget);
+    const nextBtn = document.getElementById('nextBtn');
+    nextBtn.href = event.target.dataset.href
+    items.forEach(item => {
+      item.classList.remove('selected')
+      item.style.borderColor = 'black'
+    })
+    event.target.classList.add('selected')
+    randomiseBorderColor(event.target)
+  };
+  
+  const bindButtonToClick = () => {
+    console.log('hello' )
+    items.forEach(item => {
       item.addEventListener("click", changeNextBtnHref);
-  });
-};
+    });
+  };
+  bindButtonToClick()
+}
 
-export { bindButtonToClick };
+export { selectItem };
