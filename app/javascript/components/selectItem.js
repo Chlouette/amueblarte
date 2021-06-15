685fc7abc86bae852c5ec29f513db361733df48b
James Price <jamesmpdev@gmail.com>
2021-06-15 17:54:40 +0100

    commiting so I can merge

--- app/javascript/components/selectItem.js
+++ app/javascript/components/selectItem.js
@@ -2,21 +2,20 @@ import { randomiseBorderColor } from "./randomColor";
 const selectItem = () => {
   
   const items = document.querySelectorAll(".item-card");
-  
   const changeNextBtnHref = (event) => {
     //   console.log(event.currentTarget);
     const nextBtn = document.getElementById('nextBtn');
     nextBtn.href = event.target.dataset.href
     items.forEach(item => {
-      item.classList.remove('selected')
-      item.style.borderColor = 'black'
-    })
+       item.classList.remove('selected')
+       item.classList.add('greyscale')
+       item.style.borderColor = 'black'
+     }) 
     event.target.classList.add('selected')
     randomiseBorderColor(event.target)
   };
   
   const bindButtonToClick = () => {
-    console.log('hello' )
     items.forEach(item => {
       item.addEventListener("click", changeNextBtnHref);
     });
